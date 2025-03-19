48c7a26448946000ac69601ffb64ae9a98b3da6e
Jénnifer Villalobos <jennifer.villalobos@rendalomaq.com>
2025-03-19 00:19:42 -0300

    :sparkles:

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -32,14 +32,35 @@ export default function Home() {
           animation="float-fast"
         />
       </div>
-      <div className="relative z-10 flex flex-col items-start justify-center h-[calc(100vh-200px)] mx-auto w-[1400px] max-w-full">
-        <h2 className="text-3xl font-bold text-slate-400">
-          {t('Hello, Im Julls!')}        
-        </h2>
-        <h3 className="text-5xl font-bold text-slate-400">
-          {t('and I like to create')}
-        </h3>
-        <Headline />
+      <div className="relative z-10 flex flex-col items-start justify-center h-[calc(100vh-300px)] mx-auto w-[1400px] max-w-full">
+        <div className="border-x border-slate-200">
+          <div className="relative">
+            <div className="absolute top-0 left-[-25px] text-slate-400 text-sm h-[1px] w-[25px] bg-slate-200"></div>
+            <div className="absolute bottom-0 left-[-25px] text-slate-400 text-sm h-[1px] w-[25px] bg-slate-200"></div>
+            <div className="absolute bottom-0 right-[-25px] text-slate-400 text-sm h-[1px] w-[25px] bg-slate-200"></div>
+            <div className="absolute top-0 right-[-25px] text-slate-400 text-sm h-[1px] w-[25px] bg-slate-200"></div>
+            <div className="absolute top-0 right-[-25px] text-slate-400 text-sm h-[1px] w-[25px] bg-slate-200"></div>
+            <div className="absolute top-[-25px] right-[0] text-slate-400 text-sm h-[25px] w-[1px] bg-slate-200"></div>
+            <div className="absolute top-[-25px] left-[0] text-slate-400 text-sm h-[25px] w-[1px] bg-slate-200"></div>
+            <div className="absolute top-0 left-[-25px] text-slate-400 text-sm">
+              <span className="text-slate-400 text-sm">h3</span>
+            </div>
+            <h3 className="border-t border-slate-200 p-4 text-3xl font-bold text-slate-400">
+              {t('Hello, Im Julls!')}        
+            </h3>
+          </div>
+          <div className="relative">                        
+            <div className="absolute top-0 left-[-25px] text-slate-400 text-sm">
+              <span className="text-slate-400 text-sm">h2</span>
+            </div>
+            <h2 className="border-t border-slate-200 p-4 text-5xl font-bold text-slate-400">
+              {t('and I like to create')}
+            </h2>
+            
+          </div>
+          <Headline />
+        </div>
+        
       </div>
 
       <ProjectsGrid />
